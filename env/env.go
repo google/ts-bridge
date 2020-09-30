de6351a47ef315e401069619bf4fc50cad8c960d
Artem Yakimenko <temikus@google.com>
2020-09-30 20:25:57 +1000

    Refactor env determination logic into a separate module
    This will help DRY up code later as we'll need this logic outside main too

---
+++ env/env.go
@@ -0,0 +1,17 @@
+package env
+
+import "os"
+
+// TODO(temikus): this should really be a standalone lib, something similar to https://github.com/googleapis/google-cloud-ruby/tree/master/google-cloud-env
+
+// IsAppEngine checks if the code is running in AppEngine by checking GAE_ENV variable
+func IsAppEngine() bool {
+	_, set := os.LookupEnv("GAE_ENV")
+	return set
+}
+
+// AppEngineProject returns the cloud project GAE App is running in
+// 	 Note: this is the official way to get this information within GAE.
+func AppEngineProject() string {
+	return os.Getenv("GOOGLE_CLOUD_PROJECT")
+}
