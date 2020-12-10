8ed7516d5caa313f5d395687ef88c00f3ef51473
Sophia Lin <sophialin@google.com>
2020-12-10 14:41:56 +1100

    clean up and add comments

--- create-version-tags.py
+++ create-version-tags.py
@@ -1,5 +1,6 @@
 # Used by cloudbuild.yaml to tag images with correct versions based on git version tags.
-# This script assumes that there 
+# This script assumes that there exists a file named _FULL_TAG in the container workspace
+# already, which contains the latest version number from git.
 try:
     with open("_FULL_TAG") as f:
         version = f.readline()
@@ -9,8 +10,10 @@ except FileNotFoundError:
 
 version = version.strip().split(".")
 
+# Create a temporary file in container to store the major version. E.g. "1" of v1.0.0
 with open("_MAJOR_TAG", "w") as f:
     f.write("{0}".format(version[0]))
 
+# Create a temporary file in container to store the minor version. E.g. "1.0" of v1.0
 with open("_MINOR_TAG", "w") as f:
     f.write("{0}.{1}".format(version[0], version[1]))
