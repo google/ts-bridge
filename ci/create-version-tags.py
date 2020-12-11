d1388a20ace340f0271b7f21ca5d0285e1ed5a0d
SoapHia <37109549+SoapHia@users.noreply.github.com>
2020-12-11 11:52:14 +1100

    Add Cloud Build CI to automatically build and push images to GCR (#63)
    Added cloudbuild.yaml which builds image from Dockerfile and pushes them to GCloud Container Registry with the following tags:
    - full, major and minor version
    - commit ID (prefixed with git-)
    - build ID (prefixed with build-)
    - timestamp

---
+++ ci/create-version-tags.py
@@ -0,0 +1,27 @@
+import sys
+
+# Used by cloudbuild.yaml to tag images with correct versions based on git version tags.
+# This script assumes that there exists a file named _FULL_TAG in the container workspace
+# already, which contains the latest version number from git.
+try:
+    with open("_FULL_TAG") as f:
+        version = f.read()
+except FileNotFoundError:
+    print("Please run git describe --abbrev=0 --tags > _FULL_TAG first.")
+    sys.exit()
+
+# Remove the 'v' at the start of version tags because we want numerics only
+full_version = version.lstrip('v')
+split_version = full_version.split(".")
+
+# Overwrite the full tag file so v is not included. E.g. "1.0.0" of v1.0.0
+with open("_FULL_TAG", "w") as f:
+    f.write("{0}".format(full_version))
+
+# Create a temporary file in container to store the major version. E.g. "1" of v1.0.0
+with open("_MAJOR_TAG", "w") as f:
+    f.write("{0}".format(split_version[0]))
+
+# Create a temporary file in container to store the minor version. E.g. "1.0" of v1.0
+with open("_MINOR_TAG", "w") as f:
+    f.write("{0}.{1}".format(split_version[0], split_version[1]))
