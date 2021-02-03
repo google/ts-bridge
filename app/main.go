09a98e9153afb8a32af4507b7b802665e29e9313
WizardOfFaraz <51536194+WizardOfFaraz@users.noreply.github.com>
2021-02-02 19:18:35 -0500

    minor change to logging when we fail to initialize adaptor/collector

--- app/main.go
+++ app/main.go
@@ -139,7 +139,7 @@ func main() {
 
 	metrics, err := CreateMetrics(context.Background(), config)
 	if err != nil {
-		log.Fatalf("failed creating metric dependencies: %v", err)
+		log.Fatalf("failed initializing adaptor/collector dependencies: %v", err)
 	}
 	defer cleanup(metrics)
 
